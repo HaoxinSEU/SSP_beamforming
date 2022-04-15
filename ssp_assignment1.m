77833208c49330eb40999c41bd3f52ec8b736152
HaoxinSEU <77712752+HaoxinSEU@users.noreply.github.com>
2022-04-15 17:56:22 +0200

    Update ssp_assignment1.m

--- ssp_assignment1.m
+++ ssp_assignment1.m
@@ -2,7 +2,11 @@ clear;
 clc;
 
 lambda = 0.01;  % wave length
-radius = 1;
+radius = 10;
+sigma = 0.1;   % variance of white noise
+C0 = 0.8;
+B = 2e6;    % bandwidth  
+
 r0 = [radius radius] / sqrt(2); % position of the UE, theta = 45, r = radius
 
 p = zeros(96, 2);  % position of antennas on BS, three circle with r = 0.05, 0.15 and 0.25
@@ -12,11 +16,11 @@ for i = 1 : 32
 end
 for i = 33 : 64
     alpha_i = (i-32) / 32 * 2 * pi; % angle of each antenna
-    p(i,:) = 0.055 * [cos(alpha_i) sin(alpha_i)]; 
+    p(i,:) = 0.07 * [cos(alpha_i) sin(alpha_i)]; 
 end
 for i = 65 : 96
     alpha_i = (i-64) / 32 * 2 * pi; % angle of each antenna
-    p(i,:) = 0.06 * [cos(alpha_i) sin(alpha_i)]; 
+    p(i,:) = 0.09 * [cos(alpha_i) sin(alpha_i)]; 
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -24,16 +28,19 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 w = zeros(96, 1);
 for i = 1 : 96
-    w(i) = exp(-1i * 2*pi * norm(p(i,:) - r0) / lambda);
+    w(i) = exp(-1i * 2*pi * norm(p(i,:) - r0) / lambda) / 10;
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%% beam shape for matched beamforming %%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-N = 10000;  % sample on the circle: r = 5
+N = 100000;  % sample on the circle: r = 5
 r = zeros(N, 2);
+var = 10; % Guassian distribution's variance
+interval = 50; % uniform distribution's interval
 for i = 1 : N
-    theta_i = i / N * 2 * pi;
+    %theta_i = (45 + randn * sqrt(var)) / 360 * 2 * pi;
+    theta_i = (45 + (rand-0.5) * interval) / 360 * 2 * pi;
     r(i,:) = radius * [cos(theta_i) sin(theta_i)];
 end
 
@@ -47,14 +54,19 @@ for j = 1 : N
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%% calculate SNR by AWGN channel %%%%%%%%%%%%%%
+%%%%%% calculate data rate by AWGN channel %%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+R_sample = zeros(N, 1);  % SNR of each sample
+for i = 1 : N
+    R_sample(i) = B * log2(1 + (b_gain(i) / sigma) * C0);
+end
+R_aver = mean(R_sample,'all'); % calculate the average
 
 
-
-%plot([1:1:N], b)
+%plot([1:1:N], b_gain)
+scatter(r(:,1),r(:,2))
 %db_b_gain = 10 * log(b_gain);
 %rmin = min(db_b_gain);
 %rmax = max(db_b_gain);
-polarplot([1:1:N] / N * 2*pi,b_gain)
+%polarplot([1:1:N] / N * 2*pi,b_gain)
 %rlim([rmin rmax])
